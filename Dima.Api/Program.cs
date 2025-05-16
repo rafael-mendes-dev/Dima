5cb0e9d43c821bd4760eced99e9af1a4832dcc19
Rafael Mendes <rafaelmen.ros@gmail.com>
2025-05-16 17:14:15 -0300

    Primeiro commit

---
+++ Dima.Api/Program.cs
@@ -0,0 +1,25 @@
+using Dima.Api.Data;
+using Dima.Api.Handlers;
+using Dima.Core.Handlers;
+using Microsoft.EntityFrameworkCore;
+
+var builder = WebApplication.CreateBuilder(args);
+
+var cnnStr = builder.Configuration.GetConnectionString("DefaultConnection") ?? string.Empty;
+
+builder.Services.AddDbContext<AppDbContext>(x => x.UseSqlServer(cnnStr));
+
+builder.Services.AddEndpointsApiExplorer();
+builder.Services.AddSwaggerGen(x => { x.CustomSchemaIds(type => type.FullName); });
+builder.Services.AddTransient<ICategoryHandler, CategoryHandler>();
+
+var app = builder.Build();
+
+app.UseSwagger();
+app.UseSwaggerUI();
+
+
+app.MapGet("/", () => "Hello World!");
+
+
+app.Run();
---
+++ Dima.Core/Requests/Categories/CreateCategoryRequest.cs
@@ -0,0 +1,13 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace Dima.Core.Requests.Categories;
+
+public class CreateCategoryRequest : Request
+{
+    [Required(ErrorMessage = "Titulo inválido")]
+    [StringLength(80, ErrorMessage = "O título deve ter no máximo 80 caracteres")]
+    public string Title { get; set; } = string.Empty;
+    
+    [Required(ErrorMessage = "Descrição inválida")]
+    public string Description { get; set; } = string.Empty;
+}
