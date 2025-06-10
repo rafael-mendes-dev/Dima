4266ac9c831fd3f46024eb53997a3aa986ecdce3
Rafael Mendes <rafaelmen.ros@gmail.com>
2025-06-10 20:52:19 -0300

    Finalizado endpoint de gastos por categoria.

---
+++ Dima.Api/Endpoints/Reports/GetExpensesByCategoryEndpoint.cs
@@ -0,0 +1,28 @@
+﻿using System.Security.Claims;
+using Azure;
+using Dima.Api.Common.Api;
+using Dima.Core.Handlers;
+using Dima.Core.Models.Reports;
+using Dima.Core.Requests.Reports;
+
+namespace Dima.Api.Endpoints.Reports;
+
+public class GetExpensesByCategoryEndpoint : IEndpoint
+{
+    public static void Map(IEndpointRouteBuilder app)
+        => app.MapGet("/expenses", HandleAsync)
+            .Produces<Response<List<ExpensesByCategory>?>>();
+
+    private static async Task<IResult> HandleAsync(
+        ClaimsPrincipal user,
+        GetExpensesByCategoryRequest request,
+        IReportHandler handler)
+    {
+        request.UserId = user.Identity?.Name ?? string.Empty;
+        var result = await handler.GetExpensesByCategoryReportAsync(request);
+        return result.IsSucess
+            ? TypedResults.Ok(result)
+            : TypedResults.BadRequest(result);
+    }
+    
+}
