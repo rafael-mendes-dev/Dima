0173d24356f9f6b94077bb1b8d5f86db464b6a2d
Rafael Mendes <rafaelmen.ros@gmail.com>
2025-06-10 21:30:10 -0300

    Registrado endpoints

--- Dima.Api/Common/Api/BuilderExtension.cs
+++ Dima.Api/Common/Api/BuilderExtension.cs
@@ -48,5 +48,6 @@ public static class BuilderExtension
     {
         builder.Services.AddTransient<ICategoryHandler, CategoryHandler>();
         builder.Services.AddTransient<ITransactionHandler, TransactionHandler>();
+        builder.Services.AddTransient<IReportHandler, ReportHandler>();
     }
 }
--- Dima.Api/Endpoints/Endpoint.cs
+++ Dima.Api/Endpoints/Endpoint.cs
@@ -1,6 +1,7 @@
 ﻿using Dima.Api.Common.Api;
 using Dima.Api.Endpoints.Categories;
 using Dima.Api.Endpoints.Identity;
+using Dima.Api.Endpoints.Reports;
 using Dima.Api.Endpoints.Transactions;
 using Dima.Api.Models;
 
@@ -36,6 +37,13 @@ public static class Endpoint
         endpoints.MapGroup("v1/identity").WithTags("Identity")
             .MapEndpoint<LogoutEndpoint>()
             .MapEndpoint<GetRolesEndpoint>();
+
+        endpoints.MapGroup("v1/reports").WithTags("Reports")
+            .RequireAuthorization()
+            .MapEndpoint<GetIncomesAndExpensesEndpoint>()
+            .MapEndpoint<GetIncomesByCategoryEndpoint>()
+            .MapEndpoint<GetFinancialSummaryEndpoint>()
+            .MapEndpoint<GetExpensesByCategoryEndpoint>();
     }
 
     private static IEndpointRouteBuilder MapEndpoint<TEndpoint>(this IEndpointRouteBuilder app)
